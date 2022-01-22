6cb252c87f565d920de5f4ab85347953a2c508fb
angelantonov213 <angel.antonov213@gmail.com>
2022-01-22 15:48:54 +0200

    swiftlint.yml (#2)
    * swiftlint.yml
    
    * Fix swift lint complaints
    
    * Fix swift lint complaints
    
    * Fix swift lint complaints
    
    Co-authored-by: Angel Antonov <>

--- upskilldevopsiosTests/UpskilldevopsiosTests.swift
+++ upskilldevopsiosTests/UpskilldevopsiosTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import upskilldevopsios
 
-class upskilldevopsiosTests: XCTestCase {
+class UpskilldevopsiosTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
--- upskilldevopsiosUITests/LaunchTests.swift
+++ upskilldevopsiosUITests/LaunchTests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-class upskilldevopsiosUITestsLaunchTests: XCTestCase {
+class LaunchTests: XCTestCase {
 
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
