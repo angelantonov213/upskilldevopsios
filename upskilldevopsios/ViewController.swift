22501ce460db9602219bcef2ff47a70fdce3fd2a
angelantonov213 <angel.antonov213@gmail.com>
2022-01-22 13:58:53 +0200

    Initial Xcode project (#1)
    Co-authored-by: Angel Antonov <>

---
+++ upskilldevopsios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  upskilldevopsios
+//
+//  Created by Angel Antonov on 22.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
