18b9401b6227d99f17217ea88ceff91311b4ca85
angelantonov213 <angel.antonov213@gmail.com>
2022-01-23 18:04:18 +0200

    Added functionality to add ones (#4)
    * Added functionality to add ones
    
    * Added tests
    
    * Set sonar test folders
    
    * Improve sonar propertise file
    
    * Improve sonar propertise file
    
    * Improve sonar propertise file
    
    * Improve sonar propertise file
    
    * Make swiftlint happy
    
    * Make swiftlint happy. Add back sonar props file
    
    * Remove pr.yml
    
    * Make swiftlint happy
    
    Co-authored-by: Angel Antonov <>

--- upskilldevopsios/ViewController.swift
+++ upskilldevopsios/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lblResult: UILabel!
+    @IBOutlet var btnAdd: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func btnAddTapped(_ sender: Any) {
+        var currentValue: Int = Int(lblResult.text ?? "0") ?? 0
+        let result = currentValue + 1
+
+        lblResult.text = String(result)
+    }
 
 }
-
---
+++ upskilldevopsiosTests/ViewControllerTests.swift
@@ -0,0 +1,40 @@
+//
+//  UIViewControllerTests.swift
+//  upskilldevopsiosTests
+//
+//  Created by Angel Antonov on 23.01.22.
+//
+
+import XCTest
+@testable import upskilldevopsios
+
+class ViewControllerTests: XCTestCase {
+
+    var sut: ViewController!
+
+    override func setUpWithError() throws {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+
+        sut = storyboard.instantiateViewController(withIdentifier: "ViewController") as? ViewController
+        sut.loadViewIfNeeded()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+
+    func testbtnADdInitialValue() {
+        XCTAssertEqual(sut.btnAdd.title(for: .normal), "Plus")
+    }
+
+    func testlblResultInitialValue() {
+        XCTAssertEqual(sut.lblResult.text, "0")
+    }
+
+    func testBtnAddTapped() {
+        sut.btnAddTapped(self)
+        
+        XCTAssertEqual(sut.lblResult.text, "1")
+    }
+
+}
